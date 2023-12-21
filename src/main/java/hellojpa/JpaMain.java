744eacf47b9fd32f65a784a7d65b9fb828db7425
Donghee-Jeong <weg9511@gmail.com>
2023-12-21 23:46:13 +0900

    회원 조회 예제

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,11 +15,9 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setId(1L);
-            member.setName("hello");
-
-            em.persist(member);
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println("findMember.getId() = " + findMember.getId());
+            System.out.println("findMember.getName() = " + findMember.getName());
 
             tx.commit();
         } catch (Exception e) {
