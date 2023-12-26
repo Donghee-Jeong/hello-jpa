87d8406785999f22690fe0d5ec4e1802e1896803
Donghee-Jeong <weg9511@gmail.com>
2023-12-26 23:32:18 +0900

    컬렉션 값 연관 경로 표현식

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -13,17 +13,22 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            em.persist(team);
+
             Member member1 = new Member();
             member1.setUsername("member1");
             member1.setAge(10);
+            member1.setTeam(team);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("member2");
             member2.setAge(20);
+            member2.setTeam(team);
             em.persist(member2);
 
-            String query = "select function('group_concat', m.username) from Member m";
+            String query = "select m.username From Team t join t.members m";
             List<String> results = em.createQuery(query, String.class)
                     .getResultList();
 
