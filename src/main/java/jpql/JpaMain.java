992efc82600ec827d8b35fe8153df022a4186b94
Donghee-Jeong <weg9511@gmail.com>
2023-12-27 22:15:04 +0900

    fetch 조인 예제

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -13,27 +13,39 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
 
             Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setAge(10);
-            member1.setTeam(team);
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setAge(20);
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
 
-            String query = "select m.username From Team t join t.members m";
-            List<String> results = em.createQuery(query, String.class)
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
+
+            em.flush();
+            em.clear();
+
+            String query = "select m from Member m join fetch m.team";
+            List<Member> results = em.createQuery(query, Member.class)
                     .getResultList();
 
-            for (String s : results) {
-                System.out.println("s = " + s);
+            for (Member m : results) {
+                System.out.println("m = " + m + " " + m.getTeam().getName());
             }
 
             tx.commit();
--- src/main/java/jpql/Member.java
+++ src/main/java/jpql/Member.java
@@ -9,7 +9,7 @@ public class Member {
     private Long id;
     private String username;
     private int age;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id")
     private Team team;
 
